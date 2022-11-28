dfb51db32a7d88e9e3e9b19ca93edcfdcf87f3f6
Florian Dreyer <flodreyer@web.de>
2022-11-28 15:22:37 +0100

    added recipe.js, finished stuff.js

--- stuff.js
+++ stuff.js
@@ -1,6 +1,13 @@
-let sugar
-let otherStuff
-sugar = 1
-otherStuff = 3 * sugar + 5
-console.log(otherStuff)
-console.log("\n")
+let firstName ="Florian"
+let lastName = "Dreyer"
+let fullName = firstName + " " + lastName
+
+console.log("Hello, my name is", fullName, "\n")
+
+let age = 30
+// Formally we would set a new Date() here and get the full year from that but that returns an integer iirc so i skipped this part
+let currentYear = 2022
+let birthYear = currentYear - age
+
+// Technically vague since I could not have had my birthday this year
+console.log("Since I am", age, "years old and the current year is ", currentYear, ", that means I must have been born in ", birthYear, ".\n")
