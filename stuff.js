c26ff63e7af74b71e1b284fae6750253322b51e8
Florian Dreyer <flodreyer@web.de>
2022-11-28 16:27:50 +0100

    food for thought regarding the first task

--- stuff.js
+++ stuff.js
@@ -10,4 +10,12 @@ let currentYear = 2022
 let birthYear = currentYear - age
 
 // Technically vague since I could not have had my birthday this year
+// let today = new Date()
+// let birthday = new Date(1992,12,12)
+// let timedif = today - birthday
+// let actualAge = new Date(timedif).getFullYear() - 1970
+// // and then we could do
+// let actualBirthYear = today.getFullYear() - actualAge
+// // but this uses the actual birthday to get the age which makes this whole thing irrelevant since we would have the birthyear then by default
+
 console.log("Since I am", age, "years old and the current year is ", currentYear, ", that means I must have been born in ", birthYear, ".\n")
